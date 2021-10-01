843fc2815a183545d4d31f37d553d8e7265e4c5d
danthu501 <91268795+danthu501@users.noreply.github.com>
2021-10-01 14:19:07 +0200

    Test se konton

--- Inviduellt Projekt/Program.cs
+++ Inviduellt Projekt/Program.cs
@@ -17,13 +17,33 @@ namespace Inviduellt_Projekt
 
                 
                 var Users = new List<string> { "Daniel", "Martin", "Christian", "Neo", "Jennie" };//https://channel9.msdn.com/Series/CSharp-101/CSharp-Sort-Search-and-Index-Lists                
-                var Passwords = new List<string> { "1234", "2345", "3456", "4567", "5678" };
-            
+                int[] Passwords = new int[] { 1234, 2345, 3456, 4567, 5678 };
+                
+                string[] BankAcountNames = new string [] { "Lönekonto", "Lönekonto", "Lönekonto" };
+                double[] BankBalance = new double[] { 1235.54, 1531, 1531, 56468 };
+
+                string[] BankAcountNames1 = new string[] { "Lönekonto2", "Lönekonto2", "Lönekonto2" };
+                double[] BankBalance1 = new double[] { 1254, 1531, 1531, 56468 };
+
                 Console.Write("Var god ange ditt användarnamn: ");
                 string User = Console.ReadLine();
+                foreach (var item in Users)
+                {
+                    if (User == item)
+                    {
+                        break;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Det angivna anvädarnamnet finns inte");
+                    }
+                    
+                }
+                
                 var index = Users.IndexOf(User);
-              
-
+                Console.WriteLine("Hej!" + Users[index]);
+                Console.Write("Var god ange din pinkod: ");
+                int.TryParse(Console.ReadLine(), out int GivenPassword);
 
                 //string[] Password = new string[] { "1234","2345", "3456", "4567", "5678" };
                 foreach (var item in Users)
@@ -31,16 +51,10 @@ namespace Inviduellt_Projekt
 
                     if (User == item)
                     {
-
-                        Console.WriteLine("Hej!" + User);
-                        Console.WriteLine("Var god ange din pinkod: ");
-                        string GivenPassword = Console.ReadLine();
-
                         for (int i = 0; i < 2; i++)
                         {
                             if (GivenPassword == Passwords[index])
                             {
-                                MinBool = false;
                                 bool MenyBool = true;
                                 while (MenyBool)
                                 {
@@ -55,31 +69,40 @@ namespace Inviduellt_Projekt
                                     {
                                         case 1:
                                             Console.WriteLine("Se dina konton och saldo");
+                                            Console.WriteLine(BankAcountNames1[index]);
+                                            Console.WriteLine(BankBalance1[index] + "kr");
+                                            Console.WriteLine(BankAcountNames[index]);
+                                            Console.WriteLine(BankBalance[index]+"kr");
                                             break;
                                         case 2:
                                             Console.WriteLine("Överföring mellan konton");
-                                            break;
+                                            Console.WriteLine("Vilket konto vill du ta pengar från?");
+                                            string Withdraw = Console.ReadLine();
+                                                                                                                        
+                                            Console.WriteLine("Vilket konto vill du flytta till?");
+                                            Console.WriteLine("Här är de nya saldona");
+                                                    break;
                                         case 3:
                                             Console.WriteLine("Ta ut pengar");
                                             break;
                                         case 4:
                                             Console.WriteLine("Logga ut");
                                             MenyBool = false;
+                                            
                                             break;
                                         default:
                                             Console.WriteLine("Du har angivit ett ogiltigt val");
                                             break;
                                     }
-                                }    MinBool = true;
+                                }    
                             }
                             else
                             {
                                 Console.WriteLine("Fel pinkod försök igen");
                                 Console.Write("Var god ange din pinkod: ");
-                                GivenPassword = Console.ReadLine();
-
+                                int.TryParse(Console.ReadLine(), out GivenPassword);
                             }
-                            
+                         MinBool = false;
                         }
                         
                         
